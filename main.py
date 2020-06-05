6a20b505755e108e40bec66f03247815374ab9a1
valdei777 <56629734+valdei777@users.noreply.github.com>
2020-06-05 08:03:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import requests
 from pprint import pprint
 # 702a9c45c5c1c4d8ca215af4db70ee75dd5fff3d
-
-main_link = 'https://api.github.com'
+# https://api.github.com/search/repositories?q=
+enter_link = 'https://api.github.com'
+repo_link = 'https://api.github.com/search/repositories'
 token = '702a9c45c5c1c4d8ca215af4db70ee75dd5fff3d'
-params = {'Authorization': token}
-response = requests.get(main_link, params)
 
-pprint(response)
+params = {'Authorization':token,
+          'q':'user:valdei777'}
+response = requests.get(repo_link, params=params)
+
+pprint(response.text)
 print(1)
