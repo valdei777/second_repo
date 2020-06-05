dde1771a7abed25c3203245f7165061c6de291d8
valdei777 <56629734+valdei777@users.noreply.github.com>
2020-06-05 06:51:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
 import requests
-import pprint
+from pprint import pprint
 # 702a9c45c5c1c4d8ca215af4db70ee75dd5fff3d
 
-main_link = 'https://api.github.com/graphql'
+main_link = 'https://api.github.com'
 token = '702a9c45c5c1c4d8ca215af4db70ee75dd5fff3d'
-coll = {'user': token,
-'repo':'status',
-'read':'repo_hook',
-'read':'org',
-'read':'public_key',
-'read':'gpg_key'}
-response = requests.get(main_link, coll)
+params = {'Authorization': token}
+response = requests.get(main_link, params)
 
-pprint(respons)
+pprint(response)
 print(1)
